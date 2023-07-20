32783987a3b5437091a3296bdd7cbee624cb9333
james <james@lomby.jp>
2023-07-20 16:14:28 +0900

    timeout feature

--- include/ros2_qos_tools/qos_lookup.hpp
+++ include/ros2_qos_tools/qos_lookup.hpp
@@ -1,40 +1,55 @@
 #ifndef ROS2_QOS_TOOLS_QOS_LOOKUP_HPP
 #define ROS2_QOS_TOOLS_QOS_LOOKUP_HPP
 
+#include <chrono>
+
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/qos.hpp>
 
 namespace ros2_qos_tools
 {
-    inline rclcpp::QoS get_qos_from_endpoint_infos(const std::vector<rclcpp::TopicEndpointInfo> &endpoint_infos,
-                                                   const rclcpp::Logger &logger, const std::string &topic)
+    inline rclcpp::QoS lookup_qos(
+        const rclcpp::Node &node,
+        const std::string &topic,
+        const rclcpp::EndpointType endpoint_type,
+        const float timeout = -1.0)
     {
-        if (endpoint_infos.empty())
+        auto start_time = std::chrono::high_resolution_clock::now();
+        while (true)
         {
-            RCLCPP_WARN(logger, "No endpoints for %s", topic.c_str());
-            return rclcpp::SystemDefaultsQoS();
-        }
-        else
-        {
-            auto qos = endpoint_infos[0].qos_profile();
-
-            if (qos.history() == rclcpp::HistoryPolicy::Unknown)
+            std::vector<rclcpp::TopicEndpointInfo> endpoint_infos;
+            switch (endpoint_type)
             {
-                qos.history(rclcpp::HistoryPolicy::SystemDefault);
+            case rclcpp::EndpointType::Publisher:
+                endpoint_infos = node.get_publishers_info_by_topic(topic);
+                break;
+            case rclcpp::EndpointType::Subscription:
+                endpoint_infos = node.get_subscriptions_info_by_topic(topic);
+                break;
+            default:
+                RCLCPP_WARN(node.get_logger(), "QoS lookup: invalid endpoint type requested for topic %s", topic.c_str());
+                return rclcpp::SystemDefaultsQoS();
             }
 
-            return qos;
+            if (!endpoint_infos.empty())
+            {
+                auto qos = endpoint_infos[0].qos_profile();
+
+                if (qos.history() == rclcpp::HistoryPolicy::Unknown)
+                {
+                    qos.history(rclcpp::HistoryPolicy::SystemDefault);
+                }
+
+                return qos;
+            }
+
+            std::chrono::duration<float> dt = std::chrono::high_resolution_clock::now() - start_time;
+            if (!(0.0 < timeout < dt.count()))
+            {
+                RCLCPP_WARN(node.get_logger(), "QoS lookup: timeout for topic %s", topic.c_str());
+                return rclcpp::SystemDefaultsQoS();
+            }
         }
-    }
-
-    inline rclcpp::QoS get_sub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
-    {
-        return get_qos_from_endpoint_infos(node.get_subscriptions_info_by_topic(topic), node.get_logger(), topic);
-    }
-
-    inline rclcpp::QoS get_pub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
-    {
-        return get_qos_from_endpoint_infos(node.get_publishers_info_by_topic(topic), node.get_logger(), topic);
     }
 }
 
