6dc612236b9b18071ada23fe0f521e4c344f254e
james <james@lomby.jp>
2023-07-20 14:51:33 +0900

    set default history if not available

--- include/ros2_qos_tools/qos_lookup.hpp
+++ include/ros2_qos_tools/qos_lookup.hpp
@@ -6,32 +6,35 @@
 
 namespace ros2_qos_tools
 {
-    inline rclcpp::QoS get_sub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
+    inline rclcpp::QoS get_qos_from_endpoint_infos(const std::vector<rclcpp::TopicEndpointInfo> &endpoint_infos,
+                                                   const rclcpp::Logger &logger, const std::string &topic)
     {
-        const auto endpoint_info = node.get_subscriptions_info_by_topic(topic);
-        if (endpoint_info.empty())
+        if (endpoint_infos.empty())
         {
-            RCLCPP_WARN(node.get_logger(), "No subscribers for %s", topic.c_str());
+            RCLCPP_WARN(logger, "No endpoints for %s", topic.c_str());
             return rclcpp::SystemDefaultsQoS();
         }
         else
         {
-            return endpoint_info[0].qos_profile();
+            auto qos = endpoint_infos[0].qos_profile();
+
+            if (qos.history() == rclcpp::HistoryPolicy::Unknown)
+            {
+                qos.history(rclcpp::HistoryPolicy::SystemDefault);
+            }
+
+            return qos;
         }
+    }
+
+    inline rclcpp::QoS get_sub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
+    {
+        return get_qos_from_endpoint_infos(node.get_subscriptions_info_by_topic(topic), node.get_logger(), topic);
     }
 
     inline rclcpp::QoS get_pub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
     {
-        const auto endpoint_info = node.get_publishers_info_by_topic(topic);
-        if (endpoint_info.empty())
-        {
-            RCLCPP_WARN(node.get_logger(), "No subscribers for %s", topic.c_str());
-            return rclcpp::SystemDefaultsQoS();
-        }
-        else
-        {
-            return endpoint_info[0].qos_profile();
-        }
+        return get_qos_from_endpoint_infos(node.get_publishers_info_by_topic(topic), node.get_logger(), topic);
     }
 }
 
