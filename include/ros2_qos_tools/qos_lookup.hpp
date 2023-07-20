fa0add3d011427db61fe49cdba0b436d79ee98a5
james <james@lomby.jp>
2023-07-20 14:35:39 +0900

    config for ros2_qos_tools_INCLUDE_DIR

--- include/ros2_qos_tools/qos_lookup.hpp
+++ include/ros2_qos_tools/qos_lookup.hpp
@@ -4,31 +4,34 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/qos.hpp>
 
-rclcpp::QoS get_sub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
+namespace ros2_qos_tools
 {
-    const auto endpoint_info = node.get_subscriptions_info_by_topic(topic);
-    if (endpoint_info.empty())
+    inline rclcpp::QoS get_sub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
     {
-        RCLCPP_WARN(node.get_logger(), "No subscribers for %s", topic.c_str());
-        return rclcpp::SystemDefaultsQoS();
+        const auto endpoint_info = node.get_subscriptions_info_by_topic(topic);
+        if (endpoint_info.empty())
+        {
+            RCLCPP_WARN(node.get_logger(), "No subscribers for %s", topic.c_str());
+            return rclcpp::SystemDefaultsQoS();
+        }
+        else
+        {
+            return endpoint_info[0].qos_profile();
+        }
     }
-    else
-    {
-        return endpoint_info[0].qos_profile();
-    }
-}
 
-rclcpp::QoS get_pub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
-{
-    const auto endpoint_info = node.get_publishers_info_by_topic(topic);
-    if (endpoint_info.empty())
+    inline rclcpp::QoS get_pub_qos_from_topic(const rclcpp::Node &node, const std::string &topic)
     {
-        RCLCPP_WARN(node.get_logger(), "No subscribers for %s", topic.c_str());
-        return rclcpp::SystemDefaultsQoS();
-    }
-    else
-    {
-        return endpoint_info[0].qos_profile();
+        const auto endpoint_info = node.get_publishers_info_by_topic(topic);
+        if (endpoint_info.empty())
+        {
+            RCLCPP_WARN(node.get_logger(), "No subscribers for %s", topic.c_str());
+            return rclcpp::SystemDefaultsQoS();
+        }
+        else
+        {
+            return endpoint_info[0].qos_profile();
+        }
     }
 }
 
